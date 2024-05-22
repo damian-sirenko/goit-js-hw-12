4438cf1d5b25f7c4508e33351fdde3e5cf64f4cd
damian-sirenko <152203355+damian-sirenko@users.noreply.github.com>
2024-05-22 18:19:21 +0000

    Deploying to gh-pages from @ damian-sirenko/goit-js-hw-12@96ffdd5cb85f9421486305913273fba6d24f2361 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import{S as d,i as c}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const m=o=>o.map(({id:s,webformatURL:r,largeImageURL:a,tags:e,likes:t,views:i,comments:p,downloads:u})=>`<div class="gallery-item">
-          <a class="gallery-link" href="${a}">
-            <img src="${r}" alt="${e}" class="gallery-img" id="${s}" />
+import{a as u,S as w,i as l}from"./assets/vendor-c493984e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=o(e);fetch(e.href,s)}})();const g=r=>r.map(({id:t,webformatURL:o,largeImageURL:i,tags:e,likes:s,views:c,comments:L,downloads:b})=>`<div class="gallery-item">
+          <a class="gallery-link" href="${i}">
+            <img src="${o}" alt="${e}" class="gallery-img" id="${t}" />
           </a>
           <div class="img-props">
             <div class="item-props">
               <h3 class="item-title-props">Likes</h3>
-              <p class="item-value-props">${t}</p>
+              <p class="item-value-props">${s}</p>
             </div>
             <div class="item-props">
               <h3 class="item-title-props">Views</h3>
-              <p class="item-value-props">${i}</p>
+              <p class="item-value-props">${c}</p>
             </div>
             <div class="item-props">
               <h3 class="item-title-props">Comments</h3>
-              <p class="item-value-props">${p}</p>
+              <p class="item-value-props">${L}</p>
             </div>
             <div class="item-props">
               <h3 class="item-title-props">Downloads</h3>
-              <p class="item-value-props">${u}</p>
+              <p class="item-value-props">${b}</p>
             </div> 
           </div>
-        </div>`).join(""),h="43948789-3afe6d8ecc2e5aa705550f4b3",f="https://pixabay.com/api/",y=(o="")=>{const s=new URLSearchParams({key:h,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${f}?${s}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})},g=document.querySelector(".search-form"),l=document.querySelector(".gallery"),n=document.querySelector(".loader"),v=new d(".gallery a",{captionsData:"alt",captionDelay:250});function L(o){o.preventDefault();const s=o.target.elements.search.value.trim();if(s===""){l.innerHTML="",o.target.reset(),c.show({message:"Input field can not be empty",position:"topRight",timeout:2e3,color:"red"});return}l.innerHTML="",n.classList.remove("is-hidden"),y(s).then(r=>{r.total===0&&c.show({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:2e3,color:"red"}),l.innerHTML=m(r.hits.slice(0,9)),v.refresh()}).catch(function(r){console.log(r)}).finally(()=>{o.target.reset(),n.classList.add("is-hidden")})}g.addEventListener("submit",L);
+        </div>`).join(""),S="43948789-3afe6d8ecc2e5aa705550f4b3";u.defaults.baseURL="https://pixabay.com/api/";const f=async(r,t)=>(await u.get("/",{params:{key:S,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,page:t,per_page:15}})).data,E=document.querySelector(".search-form");let h=document.querySelector(".gallery");const n=document.querySelector(".loader"),a=document.querySelector(".load-more-btn");let y=0,p="",d=1;const m=15,P=async r=>{r.preventDefault();const t=r.currentTarget;if(p=t.elements.search.value.trim(),h.innerHTML="",n.classList.remove("is-hidden"),d=1,p===""){l.show({message:"Input field can not be empty",position:"topRight",timeout:2e3,color:"red"}),t.reset(),n.classList.add("is-hidden");return}try{const{total:o,hits:i}=await f(p,d);if(o===0){l.show({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:2e3,color:"red"}),n.classList.add("is-hidden"),a.classList.add("btn-is-hidden");return}h.insertAdjacentHTML("beforeend",g(i)),a.classList.remove("btn-is-hidden"),y=Math.ceil(o/m),i.length<m?(a.classList.add("btn-is-hidden"),l.show({message:"We're sorry, but you've reached the end of search results.",position:"topRight",timeout:2e3,color:"red"})):a.classList.remove("btn-is-hidden"),v.refresh()}catch{l.show({message:"An error occurred while fetching images",position:"topRight",timeout:2e3,color:"red"})}finally{n.classList.add("is-hidden")}t.reset()},q=()=>{h.querySelector(".gallery-item:last-child").getBoundingClientRect().height,window.scrollBy({top:1e3,left:0,behavior:"smooth"})};async function M(r){try{d+=1,n.classList.remove("is-hidden");const{hits:t}=await f(p,d);h.insertAdjacentHTML("beforeend",g(t)),v.refresh(),q(),d>=y&&(a.classList.add("btn-is-hidden"),l.show({message:"We're sorry, but you've reached the end of search results.",position:"topRight",timeout:2e3,color:"red"}))}catch{l.show({message:"An error occurred while fetching more images.",position:"topRight",timeout:2e3,color:"red"}),a.classList.add("btn-is-hidden")}finally{n.classList.add("is-hidden")}}const v=new w(".gallery a",{captionsData:"alt",captionDelay:250});a.addEventListener("click",M);E.addEventListener("submit",P);
 //# sourceMappingURL=commonHelpers.js.map
